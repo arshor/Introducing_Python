0a0ccbfa38e572dfcd32c4844e60a3c46990b7f8
arshor <arshor@rambler.ru>
2016-12-28 21:38:22 +0500

    Глава 2

---
+++ 2_glava.py
@@ -0,0 +1,14 @@
+# 1
+print(60 * 60)
+# 2
+seconds_per_hour = 60 * 60
+print(seconds_per_hour)
+# 3
+print(seconds_per_hour * 24)
+# 4
+seconds_per_day = seconds_per_hour * 24
+print(seconds_per_day)
+# 5
+print(seconds_per_day / seconds_per_hour)
+# 6
+print(seconds_per_day // seconds_per_hour)
