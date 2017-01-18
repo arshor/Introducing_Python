ce9f4574235213dae5fc60c8d6f5721eb754bb43
arshor <arshor@rambler.ru>
2017-01-18 20:22:10 +0500

    Глава 6 - окончание

--- 6_glava.py
+++ 6_glava.py
@@ -21,7 +21,7 @@ class Element():
         self.symbol = symbol
         self.number = number
     def __str__(self):
-        return self.name + self.symbol + str(self.number)
+        return self.name +' ' + self.symbol + ' ' + str(self.number)
 ex1 = Element('Hydrogen', 'H', 1)
 # 5
 dict1 = {'name': 'Hydrogen', 'symbol': 'H', 'number': 1}
@@ -30,7 +30,65 @@ hydrogen = Element(**dict1)
 # hydrogen.dump()
 # 7
 print(hydrogen)
+# 8
+class Element1():
+    def __init__(self, name, symbol, number):
+        self.__name = name
+        self.__symbol = symbol
+        self.__number = number
+
+    def get_name(self):
+        return self.__name
+ex2 = Element1('Hydrogen', 'H', 1)
+print(ex2.get_name())
+# 9
 
 
+class Bear():
+    def eats(self):
+        return 'berries'
 
 
+class Rabbit():
+    def eats(self):
+        return 'clover'
+
+
+class Octothorpe():
+    def eats(self):
+        return 'campers'
+
+b = Bear()
+r = Rabbit()
+o = Octothorpe()
+print(b.eats(), r.eats(), o.eats())
+# 10
+
+
+class Laser():
+    def does(self):
+        return 'disintegrate'
+
+
+class Claw():
+    def does(self):
+        return 'crush'
+
+
+class SmartPhone():
+    def does(self):
+        return 'ring'
+
+class Robot():
+    def __init__(self, laser, claw, smartPhone):
+        self.laser = laser
+        self.claw = claw
+        self.smartPhone = smartPhone
+    def does(self):
+        print(self.laser.does(), self.claw.does(), self.smartPhone.does())
+
+l = Laser()
+c = Claw()
+s = SmartPhone()
+rob = Robot(l, c, s)
+rob.does()
